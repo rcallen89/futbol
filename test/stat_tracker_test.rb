8d131c4a0458ca79946f3838b69f926c4ea83f3c
jerry <jerry@jerrys-MacBook-Pro.local>
2019-12-17 21:49:41 -0700

    Add test for team count and games

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,5 +1,7 @@
-require_relative 'test_helper'
-require_relative '../lib/stat_tracker'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'CSV'
+require_relative '../lib/stat_tracker.rb'
 require 'pry'
 
 class StatTrackerTest < Minitest::Test
@@ -19,15 +21,53 @@ class StatTrackerTest < Minitest::Test
 
 	def test_it_exists
 		assert_instance_of StatTracker, @stat_tracker
-		binding.pry
 	end
 
 	def test_stat_tracker_attributes
-		assert_instance_of Array, @stat_tracker.seasons
-		assert_instance_of Array, @stat_tracker.teams
-		assert_instance_of Season, @stat_tracker.seasons.first
-		assert_instance_of Season, @stat_tracker.seasons.last
-		assert_instance_of Team, @stat_tracker.teams.first
-		assert_instance_of Team, @stat_tracker.teams.last
+		skip
+		assert_includes @stat_tracker.games, ["2012030221",
+ "20122013",
+ "Postseason",
+ "5/16/13",
+ "3",
+ "6",
+ "2",
+ "3",
+ "Toyota Stadium",
+ "/api/v1/venues/null"]
+		assert_includes @stat_tracker.teams, ["1",
+ "23",
+ "Atlanta United",
+ "ATL",
+ "Mercedes-Benz Stadium",
+ "/api/v1/teams/1"]
+		assert_includes @stat_tracker.game_teams, ["2012030221",
+ "3",
+ "away",
+ "LOSS",
+ "OT",
+ "John Tortorella",
+ "2",
+ "8",
+ "44",
+ "8",
+ "3",
+ "0",
+ "44.8",
+ "17",
+ "7"]
+
 	end
-end
+
+	def test_count_of_teams
+		assert_equal 32, @stat_tracker.count_of_teams
+	end
+
+	def test_games_returns_all_games
+		assert_includes @stat_tracker.games, @stat_tracker.seasons.sample.games_by_type.values.sample.sample
+	end
+
+	def test_total_games_for_team_scores
+	end
+		
+end		
