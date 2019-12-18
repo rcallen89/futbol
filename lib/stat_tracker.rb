669834e35c6e33cef0f1e1123c0253f92cba97f9
jerry <jerry@jerrys-MacBook-Pro.local>
2019-12-17 20:07:07 -0700

    Add best/worst offense

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -87,9 +87,30 @@ class StatTracker
 	end
         
         def average_total_offensive_efficiency(id)
-                all_goals_scored_all_time(id).to_f / games_played_all_time(id).to_f
-        end.round(4)
+              total = all_goals_scored_all_time(id).to_f / games_played_all_time(id).to_f
+		total.round(4)
+        end
 
-        def
 
+	def all_teams_average_offense
+		hash = {}
+		team_ids.map do |team|
+			hash[team.to_s] = average_total_offensive_efficiency(team)
+		end
+		hash
+	end 
+
+	def best_offense
+		id_of_team = all_teams_average_offense.key(all_teams_average_offense.values.max)
+		team = teams.find {|team| team.id == id_of_team.to_i}
+                team.name
+	end
+	
+	def worst_offense
+		id_of_team = all_teams_average_offense.key(all_teams_average_offense.values.min)
+		team = teams.find {|team| team.id == id_of_team.to_i}
+		team.name
+	end
+		
+			
 end
